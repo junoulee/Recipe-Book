97071003cdb8ce6c6cc95515a221939af152abfc
Junou Lee <junoulee@gmail.com>
2023-01-11 17:54:44 +0000

    added search functionality

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ var searchKeyword = document.querySelector('#site-search');
 var searchButton = document.querySelector('#search-button');
 
 function getResults(event) {
-
-  // var searchValue = searchKeyword.value;
+  var searchValue = searchKeyword.value;
+  if (searchValue === '') {
+    return;
+  }
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.edamam.com/search?q=pasta&app_id=a1100983&app_key=e2ed55cd9fd501a90232f2085e84df8c');
+  xhr.open('GET', 'https://api.edamam.com/search?q=' + searchValue + '&app_id=a1100983&app_key=e2ed55cd9fd501a90232f2085e84df8c');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     // console.log(xhr.status);
@@ -14,6 +16,7 @@ function getResults(event) {
     // console.log(searchValue);
   });
   xhr.send();
+
 }
 
 function clickResults(event) {
