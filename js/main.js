3ad43e5f6e5b0d37545d7cff41d9c620e806edd5
Junou Lee <junoulee@gmail.com>
2023-01-12 21:43:17 +0000

    pending-approval issue-2-view-search-results

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-// var results = document.querySelector('.matching-results');
+var matchingResults = document.querySelector('.matching-results');
 var searchKeyword = document.querySelector('#site-search');
 var searchButton = document.querySelector('#search-button');
+var numberOfHits = document.querySelector('.results-text');
 
-function getResults(event) {
+function getResults() {
   var searchValue = searchKeyword.value;
   if (searchValue === '') {
     return;
@@ -11,11 +12,21 @@ function getResults(event) {
   xhr.open('GET', 'https://api.edamam.com/search?q=' + searchValue + '&app_id=a1100983&app_key=e2ed55cd9fd501a90232f2085e84df8c');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
+    numberOfHits.textContent = xhr.response.hits.length + ' Matching results for ' + searchValue;
     // console.log(xhr.status);
     // console.log(xhr.response);
-    // console.log(searchValue);
+    // console.log(xhr.response.hits[0].recipe.calories);
+    dataLoop(xhr.response.hits);
+
   });
+
   xhr.send();
+  var resultsDisplayed = document.querySelectorAll('li');
+  for (var i = 0; i < resultsDisplayed.length; i++) {
+    if (resultsDisplayed) {
+      resultsDisplayed[i].remove();
+    }
+  }
 
 }
 
@@ -28,22 +39,43 @@ function clickResults(event) {
 searchKeyword.addEventListener('search', getResults);
 searchButton.addEventListener('click', clickResults);
 
-// function renderResult(result) {
-//   var bullets = document.createElement('li');
-//   bullets.classList.add('recipe-card');
+function renderResult(result) {
 
-//   var resultColumn = document.createElement('div');
-//   resultColumn.classList.add('column-third');
-//   bullets.appendChild(resultColumn);
+  var bullets = document.createElement('li');
+  bullets.classList.add('recipe-card');
 
-//   var recipePic = document.createElement('img');
-//   recipePic.classList.add('result-image');
-//   recipePic.setAttribute('src', result.hits.recipe.url);
-//   resultColumn.appendChild(recipePic);
+  var resultColumn = document.createElement('div');
+  resultColumn.classList.add('column-third');
+  bullets.appendChild(resultColumn);
 
-//   var recipeName = document.createElement('h2');
-//   recipeName.classList.add('recipe-name');
-//   recipeName.textContent = result.hits.label;
-//   resultColumn.appendChild(recipeName);
+  var recipePic = document.createElement('img');
+  recipePic.classList.add('result-image');
+  recipePic.setAttribute('src', result.image);
+  resultColumn.appendChild(recipePic);
 
-// }
+  var recipeName = document.createElement('h3');
+  recipeName.classList.add('recipe-name');
+  recipeName.textContent = result.label;
+  resultColumn.appendChild(recipeName);
+
+  var source = document.createElement('h5');
+  source.classList.add('source-text');
+  source.textContent = 'Source: ' + result.source;
+  resultColumn.appendChild(source);
+
+  var fineDetails = document.createElement('h5');
+  fineDetails.classList.add('calories-servings');
+  fineDetails.textContent = Math.trunc(result.calories) + ' Calories ' + ' | ' + result.yield + ' Servings';
+  resultColumn.appendChild(fineDetails);
+
+  return bullets;
+}
+function dataLoop(result) {
+
+  for (var i = 0; i < result.length; i++) {
+    var domTree = renderResult(result[i].recipe);
+    matchingResults.appendChild(domTree);
+  }
+
+}
+document.addEventListener('DOMContentLoaded', dataLoop);
